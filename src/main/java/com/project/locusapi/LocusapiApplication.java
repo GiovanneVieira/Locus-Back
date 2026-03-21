079d702485e483fab5b313dcc53862ab9fd1a5fd
Giovanne Vieira <gigio.v.queiroz@gmail.com>
2026-03-21 06:01:51 -0300

    First Commit

---
+++ src/main/java/com/project/locusapi/LocusapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.locusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocusapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocusapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/locusapi/LocusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.locusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
